38841c295ceb204ff2918fc838713dba3f594725
Nitish Nashine <nitishnashine@gmail.com>
2024-03-23 18:57:00 +0530

    pages done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import LoginForm from "./Components/Login/Login";
+import RegisterForm from "./Components/Register/Register";
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm />
-    </div>
+    <>
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+
+        <Route path="/" element={"Hello From Home Page"} />
+        <Route path="/leatherboard" element={"Hello From Leather-Board Page"} />
+        <Route path="*" element={"404 Not Found"} />
+      </Routes>
+    </>
   );
 }
 
